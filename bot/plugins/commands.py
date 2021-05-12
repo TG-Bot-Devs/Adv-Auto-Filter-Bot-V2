41ce450b6cffa7b189447f66d8809f25fbfde165
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-12 11:20:17 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -38,7 +38,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developer', url="https://t.me/Vivek_KERALA"
                                 )
                         ]
                     ]
@@ -57,7 +57,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developer', url="https://t.mE/VIVEK_KERALA"
                                 )
                         ]
                     ]
@@ -76,7 +76,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developer', url="https://t.ME/VIVEK_KERALA"
                                 )
                         ]
                     ]
@@ -89,12 +89,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('Developer', url='https://t.me/VIVEK_KERALA')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
-    ],[
-        InlineKeyboardButton('Help ⚙', callback_data="help")
+        InlineKeyboardButton('⚙️Help', callback_data='help'),
+        InlineKeyboardButton('About👨🏻‍🎓', callback_data='about')
     ]]
     
     reply_markup = InlineKeyboardMarkup(buttons)
@@ -108,6 +106,29 @@ async def start(bot, update):
         reply_to_message_id=update.message_id
     )
 
+
+
+
+admins = [1414146649, 1298713715]
+
+@Client.on_message(filters.command(["help"]) & filters.private, group=1 & filters.users(Admins))
+async def ownerhelp(bot, update):
+    buttons = [[
+        InlineKeyboardButton('Home ⚡', callback_data='start'),
+        InlineKeyboardButton('About 🚩', callback_data='about')
+    ],[
+        InlineKeyboardButton('Close 🔐', callback_data='close')
+    ]]
+    
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.OWNER_HELP_TEXT,
+        reply_markup=reply_markup,
+        parse_mode="html",
+        reply_to_message_id=update.message_id
+    )
 
 @Client.on_message(filters.command(["help"]) & filters.private, group=1)
 async def help(bot, update):
