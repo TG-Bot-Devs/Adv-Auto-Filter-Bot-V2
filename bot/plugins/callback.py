666e94bd5a08eee5e746734b4e834a4554148020
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-12 11:28:39 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -1608,7 +1608,7 @@ async def cb_about(bot, update: CallbackQuery):
         [
             InlineKeyboardButton
                 (
-                    "My Dev ⚡", url="https://t.me/AlbertEinstein_TG"
+                    "My Dev ⚡", url="https://t.me/Vivek_KERALA"
                 ),
                 
             InlineKeyboardButton
