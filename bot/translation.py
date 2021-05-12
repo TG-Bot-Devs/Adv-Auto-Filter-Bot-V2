b85f835eddfcc3c0b2e0b7005ca4064e38bcd82a
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-12 11:05:16 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,16 +4,20 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+    START_TEXT = """<b>Hᴇʟʟᴏ {}!!</b>
+<i>I Aᴍ A Aᴜᴛᴏ Fɪʟᴛᴇʀ Bᴏᴛ....😉</i>
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+Dᴏɴ'ᴛ Wᴀsᴛᴇ ʏᴏᴜʀ Tɪᴍᴇ, I'ᴍ Sᴘᴇᴄɪᴀʟʟʏ Mᴀᴅᴇ Fᴏʀ [ΝᎬͲҒᏞᏆХ ՏᎬᎡᏆᎬՏ](https://t.me/Netflixseries006)
 
-For More Details Click Help Button Below..
-@CrazyBotsz
-</i>"""    
-    
+sᴏ, ʏᴏᴜ ᴄᴀɴ'ᴛ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 😜
+
+"""    
+
     HELP_TEXT = """
+ᴏɴʟʏ ᴍʏ ᴏᴡɴᴇʀs ᴄᴏᴜʟᴅ sᴇᴇ ᴛʜɪs...😜
+"""
+    
+    OWNER_HELP_TEXT = """
 <b><i><u>How To Use Me!?</u></i></b>
 
 <i>
@@ -42,17 +46,15 @@ For More Details Click Help Button Below..
             -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
-            
-@CrazyBotsz
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Vivek_Kerala">Vivek</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://t.me/nokkiirunnoippokittum/2">Click Me</a></i>
 """
